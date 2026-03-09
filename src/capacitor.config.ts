7b8be8db7005938fe5b97232d57c77972777065a
Ilia Prokhorov <demensdeum@gmail.com>
2026-03-09 18:20:06 +0400

    Capacitor config changed

--- src/capacitor.config.ts
+++ src/capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.helloworld',
-  appName: 'Hello World',
+  appId: 'com.demensdeum.teflecher.editor',
+  appName: 'Teflecher Editor',
   webDir: 'dist'
 };
 
